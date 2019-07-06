2899d746fb1724a5f61bfdf68dc16312ec139363
xueenwu <45607584+xueenwu@users.noreply.github.com>
2019-07-05 21:19:52 -0400

    Add files via upload

--- dpyton.py
+++ dpyton.py
@@ -1,14 +1,20 @@
 import numpy as np
  
 #6 has nice eigenvalues--7 and 8 dont
-def eigenstuff(n):
-    grap_lap = np.array([[0]*n]*n)
-    for i in range(n):
-        grap_lap[i,i] = 4
-        grap_lap[i,(i-1)%n] = -1
-        grap_lap[i,(i-2)%n] = -1
-        grap_lap[i,(i+1)%n] = -1
-        grap_lap[i,(i+2)%n] = -1
+def graph_laplacian(n):
+    grap_lap = None
+    if (n > 4):
+        grap_lap = np.array([[0]*n]*n)
+        for i in range(n):
+            grap_lap[i,i] = 4
+            grap_lap[i,(i-1)%n] = -1
+            grap_lap[i,(i-2)%n] = -1
+            grap_lap[i,(i+1)%n] = -1
+            grap_lap[i,(i+2)%n] = -1
+    else: 
+        grap_lap = np.array([[-1] * n] * n)
+        for i in range(n):
+            grap_lap[i, i] = n - 1
     print("size: " + str(n))
 #    print(grap_lap)
     #print("determinant: " + str(np.linalg.det(grap_lap)))
@@ -20,11 +26,11 @@ def product(matrix):
     eigenvalues = np.linalg.eigvals(matrix)
     product = 1
     for e in eigenvalues:
-        if (e != 0):
+        if (abs(e) >= 10 ** (-10)):
             product *= e
     return product / len(eigenvalues)
 
 
-for i in range(1, 9):
-    eigenstuff(i)
+for i in range(1, 20):
+    graph_laplacian(i)
 
